a07478cbcc99fcdd7868b385a6a2a0a9a04ec929
ANDREY SILVA DE JESUS <andrey.sjesus1@senacsp.edu.br>
2023-05-03 13:57:17 -0300

    Adjusts | WIP 28

--- Controller/tarefas/deletar.php
+++ Controller/tarefas/deletar.php
@@ -6,15 +6,15 @@ require_once('../../model/ClassModel.php');
 $json = file_get_contents('php://input');
 $reqbody = json_decode($json);
 
-$idreserve = $reqbody->$idreserve;
-print_r($idreserve); 
+$idreserve = $reqbody->idreserve;
+
 $conexao = new Conexao();
 
 $db = $conexao->abrirConexao();
 
 $usuarioModel = new UsuarioModel($db);
 
-$usuarioModel->$idreserve = $idreserve;
+$usuarioModel->idreserve = $idreserve;
 
 $dados = $usuarioModel->deletar();
 echo json_encode($dados);
